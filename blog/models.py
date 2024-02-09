eede83a20b96cd980956e0250614f152b1ff9a33
Student <elkadjk@.pl>
2024-02-09 12:19:26 +0100

    pierszy

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Opublikowany')
+    )
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    slug = models.SlugField(max_length=255,unique_for_date='publish')
